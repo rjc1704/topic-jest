4f8ba17cc354ec43595d81a738d4f9fd653810eb
Jecheon Ryu <rjc1704@gmail.com>
2025-05-02 00:29:43 +0900

    세션 기반 인가 구현

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,11 +1,11 @@
 import express from "express";
-
+import auth from "../middlewares/auth.js";
 import productService from "../services/productService.js";
 
 const productController = express.Router();
 
 // TODO: 로그인 여부를 확인하는 미들웨어를 적용함으로써 인가 처리 해보세요
-productController.post("/", async (req, res, next) => {
+productController.post("/", auth.verifySessionLogin, async (req, res, next) => {
   const createdProduct = await productService.create(req.body);
   return res.json(createdProduct);
 });
